e6afe4c1365fd2e7d3931a9c1602d440b62fc991
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-26 11:21:18 +0300

    API tests are performed successfully

--- src/test/java/APITests/week1/VerifyingSpartanDetailsByUsingPath.java
+++ src/test/java/APITests/week1/VerifyingSpartanDetailsByUsingPath.java
@@ -8,26 +8,27 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class VerifyingSpartanDetailsByUsingPath {
     @BeforeClass
-    public void setUpClass(){
-        RestAssured.baseURI ="http://54.89.14.144:8000";
+    public void setUpClass() {
+        RestAssured.baseURI = "http://54.89.14.144:8000";
     }
 
     @Test
     public void spartanTestsVerifyingSpartanDetails() {
         Response response = RestAssured.given().accept(ContentType.JSON)
-                .and().pathParam("id",28)
+                .and().pathParam("id", 28)
                 .when().get("/api/spartans/{id}");
 
         //Verify and print status code from response object
-        Assert.assertEquals(response.statusCode(),200);
+        Assert.assertEquals(response.statusCode(), 200);
         System.out.println("response.statusCode() = " + response.statusCode());
 
         //printing response content type from response object
-        Assert.assertEquals(response.contentType(),"application/json");
+        Assert.assertEquals(response.contentType(), "application/json");
         System.out.println("response.contentType() = " + response.contentType());
 
         System.out.println("path(\"id\") = " + response.body().path("id").toString());
@@ -37,10 +38,10 @@ public class VerifyingSpartanDetailsByUsingPath {
 
         System.out.println("-----------------------");
 
-        int id=response.path("id");
-        long phone=response.path("phone");
-        String name=response.body().path("name");
-        String gender=response.body().path("gender");
+        int id = response.path("id");
+        long phone = response.path("phone");
+        String name = response.body().path("name");
+        String gender = response.body().path("gender");
 
         System.out.println("id = " + id);
         System.out.println("phone = " + phone);
@@ -48,12 +49,59 @@ public class VerifyingSpartanDetailsByUsingPath {
         System.out.println("gender = " + gender);
 
         //Verify Json keys and values
-        Assert.assertEquals(id,28);
-        Assert.assertEquals(gender,"Male");
-        Assert.assertEquals(name,"Woodrow");
-        Assert.assertEquals(phone,7411659821L);
+        Assert.assertEquals(id, 28);
+        Assert.assertEquals(gender, "Male");
+        Assert.assertEquals(name, "Woodrow");
+        Assert.assertEquals(phone, 7411659821L);
 
     }
 
+    @Test
+    public void spartanTestsVerifyingSpartanDetails2() {
+        Response response = RestAssured.get("/api/spartans");
 
+        //Get details by using index like in array
+        int firstId=response.path("id[0]");
+        String firstName=response.path("name[0]");
+        String firstGender=response.path("gender[0]");
+        long firstPhone=response.path("phone[0]");
+
+        System.out.println("firstPhone = " + firstPhone);
+        System.out.println("firstGender = " + firstGender);
+        System.out.println("firstName = " + firstName);
+        System.out.println("firstId = " + firstId);
+
+        //Get lastName by using index. Index should be ( -1 )
+        String lastName=response.path("name[-1]");
+        System.out.println("lastName = " + lastName);
+
+        //Get all names only
+        List<String> allNames = response.path("name");
+        System.out.println("allNames = " + allNames);
+        System.out.println("Number of the allNames = " + allNames.size());
+
+        //Get all names only
+        List<Object> allPhoneNumbers = response.path("phone");
+        for (Object eachPhoneNumber : allPhoneNumbers) {
+            System.out.println(eachPhoneNumber);
+        }
+
+        System.out.println("-----------------------------");
+        //Dynamic way to write all details
+
+        for (int i = 0; i < allNames.size(); i++) {
+            int id=response.path("id["+i+"]");
+            String name=response.path("name["+i+"]");
+            String gender=response.path("gender["+i+"]");
+            String phone=""+response.path("phone["+i+"]");
+
+            System.out.println(i+1+". Id = " + id);
+            System.out.println(i+1+". Gender = " + gender);
+            System.out.println(i+1+". Name = " + name);
+            System.out.println(i+1+". Phone = " + phone);
+
+            System.out.println("-----------------------------");
+
+        }
+    }
 }
