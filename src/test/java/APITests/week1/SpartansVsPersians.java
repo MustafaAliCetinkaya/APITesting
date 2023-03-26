954e4d474046610ac8767530b7c39f890f8fadb6
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-26 09:44:51 +0300

    API tests are performed successfully

--- src/test/java/APITests/week1/SpartansVsPersians.java
+++ src/test/java/APITests/week1/SpartansVsPersians.java
@@ -14,7 +14,7 @@ public class SpartansVsPersians {
     }
 
     @Test
-    public void spartanTestsWithParameters1() {
+    public void spartanTestsWithPositivePathParameters() {
         Response response = RestAssured.given().accept(ContentType.JSON)
                 .and().pathParam("id",18)
                 .when().get("/api/spartans/{id}");
@@ -29,5 +29,27 @@ public class SpartansVsPersians {
 
         //Verify name whether exists in the response
         Assert.assertTrue(response.body().asString().contains("Allen"));
+
+        response.body().prettyPrint();
+    }
+
+    @Test
+    public void spartanTestsWithNegativePathParameters() {
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .and().pathParam("id",1818)
+                .when().get("/api/spartans/{id}");
+
+        //Verify and print status code from response object
+        Assert.assertEquals(response.statusCode(),404);
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        //printing response content type from response object
+        Assert.assertEquals(response.contentType(),"application/json");
+        System.out.println("response.contentType() = " + response.contentType());
+
+        //Verify name whether exists in the response
+        Assert.assertTrue(response.body().asString().contains("Not Found"));
+
+        response.body().prettyPrint();
     }
 }
