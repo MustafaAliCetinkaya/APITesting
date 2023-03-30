6b4e04b3195079ab1b6e23f210a0cd144a5fbd79
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-30 10:51:53 +0300

    API tests are performed successfully

---
+++ src/test/java/APITests/week3/AutomationExercises3.java
@@ -0,0 +1,60 @@
+package APITests.week3;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AutomationExercises3 {
+    @BeforeClass
+    public void setUpClass() {
+        RestAssured.baseURI = "https://automationexercise.com";
+    }
+
+    @Test
+    public void automationExercisesVerifyHeaders() {
+        //send a get request and save response inside the Response object
+        //Right hand side is request part. We store the answer in the response.
+        Response response = RestAssured.get("/api/productsList");//Simple string concentration for the remaining part of the URL
+
+        //Verify and print status code from response object
+        Assert.assertEquals(response.statusCode(), 200);//This comes from TestNG
+        response.then().assertThat().statusCode(200);//Another assertion way
+
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        //printing response content type from response object
+        System.out.println("response.contentType() = " + response.contentType());
+        response.then().assertThat().contentType("text/html; charset=utf-8");//Comes from restAssured
+        Assertions.assertEquals("text/html; charset=utf-8",response.contentType());//This comes from Junit5
+        System.out.println("----------------------------");
+        //Below part is about verifying headers
+        System.out.println("response.headers().toString() = " + response.headers().toString());
+        Assertions.assertTrue(response.headers().hasHeaderWithName("Date"));
+        Assertions.assertTrue(response.headers().hasHeaderWithName("NEL"));
+        Assertions.assertTrue(response.headers().hasHeaderWithName("Server"));
+        Assertions.assertTrue(response.headers().hasHeaderWithName("CF-RAY"));
+        Assertions.assertTrue(response.headers().hasHeaderWithName("status"));
+        System.out.println("----------------------------");
+        //Get a specific header from response
+        System.out.println("response.header(\"Date\") = " + response.header("Date"));
+        System.out.println("response.header(\"NEL\") = " + response.header("NEL"));
+        System.out.println("response.header(\"Server\") = " + response.header("Server"));
+        System.out.println("response.header(\"status\") = " + response.header("status"));
+        System.out.println("----------------------------");
+        //Verify header paired infos from response
+        Assertions.assertTrue(response.header("Date").contains("Thu, 30 Mar 2023"));
+        Assertions.assertEquals("{\"success_fraction\":0,\"report_to\":\"cf-nel\",\"max_age\":604800}",response.header("NEL"));
+        Assertions.assertEquals("cloudflare",response.header("Server"));
+        Assertions.assertEquals("200 OK",response.header("status"));
+    }
+
+
+}
