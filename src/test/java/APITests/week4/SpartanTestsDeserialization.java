56548aa0844c8c940f26c7a297829463c76d0ed9
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-04-03 23:48:26 +0300

    New API tests are performed successfully

--- src/test/java/APITests/week4/SpartanTestsDeserialization.java
+++ src/test/java/APITests/week4/SpartanTestsDeserialization.java
@@ -1,9 +1,9 @@
 package APITests.week4;
 
 import APITests.utilities.SpartanTestBase;
+import com.google.gson.Gson;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.testng.Assert;
@@ -27,6 +27,27 @@ public class SpartanTestsDeserialization extends SpartanTestBase {
         Assert.assertEquals(spartanPojo.getId(),15);
         Assert.assertEquals(spartanPojo.getPhone(),1938695106);
         Assert.assertEquals(spartanPojo.getName(),"Meta");
+
+    }
+    @Test
+    public void test2GsonExample() {
+        Gson gson=new Gson();
+
+        String myJsonBody= "{id=15, phone=1938695106, name='Meta', gender='Female'}";
+
+        //Convert from java to json body
+        SpartanPojo spartanPojo=gson.fromJson(myJsonBody, SpartanPojo.class);
+
+        System.out.println(spartanPojo);
+
+        //Serialization from Java Object to JSON BODY
+        SpartanPojo spartan =new SpartanPojo(101, 9798761225l,"Pala","Male");
+
+        //Converting custom class to json (Serialization)
+        String convertedBodyToJson= gson.toJson(spartan);
+        System.out.println("convertedBodyToJson = " + convertedBodyToJson);
+
+
 
     }
 }
