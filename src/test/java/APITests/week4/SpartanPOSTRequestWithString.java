2f6e7cae7d6d257281cd785818afee7be637ff35
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-04-04 11:00:49 +0300

    New API tests are performed successfully

--- src/test/java/APITests/week4/SpartanPOSTRequestWithString.java
+++ src/test/java/APITests/week4/SpartanPOSTRequestWithString.java
@@ -6,6 +6,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,33 +29,42 @@ public class SpartanPOSTRequestWithString extends SpartanTestBase {
                 .when().post("/api/spartans");
 
         //Validations / Verify Status Code is 201
-        assertEquals(response.statusCode(),201);
-        assertEquals(response.contentType(),"application/json");
+        assertEquals(response.statusCode(), 201);
+        assertEquals(response.contentType(), "application/json");
 
         //Verify request body
-        assertEquals(response.jsonPath().getString("data.name"),"Pala Remzi");
-        assertEquals(response.jsonPath().getString("data.gender"),"Male");
-        assertEquals(response.jsonPath().getLong("data.phone"),988776543324L);
+        assertEquals(response.jsonPath().getString("data.name"), "Pala Remzi");
+        assertEquals(response.jsonPath().getString("data.gender"), "Male");
+        assertEquals(response.jsonPath().getLong("data.phone"), 988776543324L);
 
     }
-    @DisplayName("ConvertJsonToCollection")
+
+    @DisplayName("POST With Map")
     @Test
-    public void test2() {
+    public void POSTWithMap() {
+//Create a map to be used as a body for post request
+
+        Map<String, Object> addSpartanWithPost=new HashMap<>();
+        addSpartanWithPost.put("name","Siavash Chan");
+        addSpartanWithPost.put("gender","Female");
+        addSpartanWithPost.put("phone",233456677889L);
+
+        //Sending json body as MAP
         Response response = (Response) given().accept(ContentType.JSON)
-                .when().get("/api/spartans");
+                .contentType("application/json")
+                .body(addSpartanWithPost)
+                .when().post("/api/spartans");
 
-        //We convert json file by using as(Map.class) to Java Collections-Map
-        List<Map <String,Object>>spartanList = response.body().as(List.class);  //De-Serialization (Collections)==> from Json to MAP
-        System.out.println("First spartan: "+spartanList.get(0));
-        System.out.println("First spartan's name: "+spartanList.get(0).get("name"));
+        //Validations / Verify Status Code is 201
+        assertEquals(response.statusCode(), 201);
+        assertEquals(response.contentType(), "application/json");
 
-        int counter=1;
-        for (Map<String, Object> eachMap : spartanList) {
-            System.out.println(counter+". spartan : "+eachMap);
-            System.out.println("--------------"+counter+" .spartan is written above"+"----------------");
-            counter++;
-        }
+        response.prettyPrint();
 
+        //Verify request body
+        assertEquals(response.jsonPath().getString("data.name"), "Siavash Chan");
+        assertEquals(response.jsonPath().getString("data.gender"), "Female");
+        assertEquals(response.jsonPath().getLong("data.phone"), 233456677889L);
 
 
     }
