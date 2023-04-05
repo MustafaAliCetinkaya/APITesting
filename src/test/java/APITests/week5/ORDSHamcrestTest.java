90b1ecd8fc2c21770e5faa67c3b051ee158bb7ff
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-04-06 00:17:44 +0300

    New API tests are performed successfully

--- src/test/java/APITests/week5/ORDSHamcrestTest.java
+++ src/test/java/APITests/week5/ORDSHamcrestTest.java
@@ -18,7 +18,7 @@ public class ORDSHamcrestTest extends HRTestBase {
     @DisplayName("GET request to Employees IT_PROG endpoint and chaining")
     @Test
     public void employeesTest(){
-        //send a get request to emplyoees endpoint with query parameter job_id IT_PROG
+        //send a get request to employees endpoint with query parameter job_id IT_PROG
         //verify each job_id is IT_PROG
         //verify first names are .... (find proper method to check list against list)
         //verify emails without checking order (provide emails in different order,just make sure it has same emails)
