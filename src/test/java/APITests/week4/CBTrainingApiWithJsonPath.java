7c017999fbbf1cade8f48bcf62994f2f9366f495
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-04-05 18:08:04 +0300

    New API tests are performed successfully

--- src/test/java/APITests/week4/CBTrainingApiWithJsonPath.java
+++ src/test/java/APITests/week4/CBTrainingApiWithJsonPath.java
@@ -4,6 +4,8 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -37,15 +39,29 @@ public class CBTrainingApiWithJsonPath {
              */
 
         Response response= given().accept(ContentType.JSON)
-                .and().pathParam("studentId",34)
-                .when().get("student/{studentId}");
+                /*.and().pathParam("studentId",34)
+                .when().get("student/{studentId}");*/
+                .when().get("student/all");
 
-        response.body().prettyPrint();
+        System.out.println("----------------studentId==34-----------------");
+        List<String> wantedStudent = response.jsonPath().getList("students.findAll {it.studentId==34}.firstName");
+        System.out.println(wantedStudent);
 
         assertEquals(200,response.statusCode());
         assertEquals("application/json;charset=UTF-8",response.header("Content-Type"));
         assertTrue(response.header("transfer-encoding").contains("chunked"));
         assertTrue(response.headers().hasHeaderWithName("date"));
 
+        System.out.println("----------------gender==Female-----------------");
+        //get me first name of students whose batch number is 22
+        List<String> empNames = response.jsonPath().getList("students.findAll {it.gender==\"Female\"}.firstName");
+        System.out.println(empNames);
+
+        System.out.println("----------------Search By Email-----------------");
+        List<String> wantedStudent2 = response.jsonPath().getList("students.findAll {it.firstName==\"Glen\"}.contact.emailAddress");
+        System.out.println(wantedStudent2);
+
+        List<String> wantedStudent3 = response.jsonPath().getList("students.findAll {it.contact.emailAddress==\"glc@gmail.com\"}.studentId");
+        System.out.println(wantedStudent3);
     }
 }
