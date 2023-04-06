f676d18ed643b54172f7f7c55af2c0428f7653b8
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-04-06 23:53:01 +0300

    New API tests are performed successfully

--- src/test/java/APITests/week6/Spartan.sql
+++ src/test/java/APITests/week6/Spartan.sql
@@ -4,6 +4,8 @@ select name from spartans where gender='Female';
 
 select * from spartans where gender='Female' and spartan_id between 101 and 111;
 
+select * from spartans where UPDATED_AT is not null;
+
 select spartan_id,name,gender,phone
 from spartans
 where spartan_id = 15;
