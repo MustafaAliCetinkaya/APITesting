5e755e50606a4bd82d3e876265338eef7a1e0dc1
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-04-06 23:44:42 +0300

    New API tests are performed successfully

--- src/test/java/APITests/week6/Spartan.sql
+++ src/test/java/APITests/week6/Spartan.sql
@@ -7,3 +7,9 @@ select * from spartans where gender='Female' and spartan_id between 101 and 111;
 select spartan_id,name,gender,phone
 from spartans
 where spartan_id = 15;
+
+select * from SPARTANS where name like 'Ma%';
+
+select * from SPARTANS where name like '%ee';
+
+select * from SPARTANS where name like '%eli%';
