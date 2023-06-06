faa7f088112c239b51f558932cf7f243cf437f17
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-06-06 09:44:07 +0300

    New API tests are performed successfully

--- src/test/java/APITests/Pojo/Employee.java
+++ src/test/java/APITests/Pojo/Employee.java
@@ -7,10 +7,10 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true)//If your POJO does not contain certain properties that JSON does contain, they are ignored and no error is thrown.
 public class Employee {
 
-    @JsonProperty("first_name")
+    @JsonProperty("first_name")//Alanlarımızın json içerisinde hangi isimle gözükeceğini belirledik.
     private String firstName;
     @JsonProperty("last_name")
     private String lastName;
